b2940b9b134a8a990b7cae4946dcf2da0ad3be56
wangkun <wb-wk228937@ant-financial.com>
2019-01-22 17:39:30 +0800

    learn lodash

--- src/entries/index.js
+++ src/entries/index.js
@@ -3,8 +3,8 @@ import { render } from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
 import App from '../pages/App/App.jsx';
 import '../common/less/index.less';
-
 import env from '../common/js/env.js';
+import '../common/js/global.js';
 
 if (env === 'development') {
   require('../mocks/index.js');
