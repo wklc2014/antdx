5fc5124f8921ba0e8795bf38e56d2be62ff55403
wangkun <wb-wk228937@ant-financial.com>
2019-04-23 14:48:40 +0800

    master

--- src/components/index.js
+++ src/components/index.js
@@ -1,21 +1,9 @@
-import HForm from './HForm/HForm.jsx';
-import HFormItem from './HForm/HFormItem.jsx';
-import getDefaultValues from './HForm/lib/getDefaultValues.js';
-import HPicture from './HPicture/HPicture.jsx';
-import HPictureWraper from './HPicture/HPictureWraper.jsx';
-import HTagGroup from './HTagGroup/HTagGroup.jsx';
-import MainLayout from './MainLayout/MainLayout.jsx';
-import WarterMark from './WarterMark/WarterMark.jsx';
+export { default as HForm } from './HForm/HForm.jsx';
+export { default as HFormItem } from './HForm/HFormItem.jsx';
+export { default as getDefaultValues } from './HForm/lib/getDefaultValues.js';
+export { default as HPicture } from './HPicture/HPicture.jsx';
+export { default as HPictureWraper } from './HPicture/HPictureWraper.jsx';
+export { default as HTagGroup } from './HTagGroup/HTagGroup.jsx';
+export { default as MainLayout } from './MainLayout/MainLayout.jsx';
+export { default as WarterMark } from './WarterMark/WarterMark.jsx';
 
-const antdx = {
-  HForm,
-  HFormItem,
-  getDefaultValues,
-  HPicture,
-  HPictureWraper,
-  HTagGroup,
-  MainLayout,
-  WarterMark,
-}
-
-export default antdx;
